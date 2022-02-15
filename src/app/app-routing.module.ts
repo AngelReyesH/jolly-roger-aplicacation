1dd467b368f23a79f7549f168010252a601754ca
ANGEL IVAN REYES HERNANDEZ <BRM15603@da.banregio.com>
2022-02-15 07:59:17 -0600

    new migration jolly roger

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { RootComponent } from './components/root/root.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: RootComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: './modules/home/home.module#HomeModule'
+      },
+     {
+       path: 'login',
+       loadChildren: './modules/auth/auth.module#AuthModule'
+     }
+
+    ]
+  },
+  {  path: '**', redirectTo: 'login' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,52 +9,7 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
-  leftContent = [
-    { content: 'Favoritos', isTitle: true },
-    { content: 'Reportes', isTitle: false},
-    { content: 'Expediente', isTitle: false},
-    { content: 'Catalogo', isTitle: false},
-    { content: 'Cotizador', isTitle: false}
-  ];
-
-  mainContent = [
-    { isHeader: false,
-      content: [
-        { content: 'Clientes', isTitle: true },
-        { content: 'CRM', isTitle: false },
-        { content: 'Prospectos', isTitle: false },
-        { content: 'ADN', isTitle: false },
-        { content: 'Calificacion', isTitle: false }
-      ]
-    },
-    { isHeader: false,
-      content: [
-        { content: 'Lorem', isTitle: true },
-        { content: 'Ipsum', isTitle: false },
-        { content: 'Ipsum', isTitle: false },
-        { content: 'Ipsum', isTitle: false },
-        { content: 'Ipsum', isTitle: false }
-      ]
-    },
-    { isHeader: false,
-      content: [
-        { content: 'Lorem', isTitle: true },
-        { content: 'Ipsum', isTitle: false },
-        { content: 'Ipsum', isTitle: false },
-        { content: 'Ipsum', isTitle: false },
-        { content: 'Ipsum', isTitle: false }
-      ]
-    }
-  ];
-  
-  activeFinder = false;
-
-  
   ngOnInit() {
-    
   }
-toggleFinder(e) {
-    console.log(e);
-    this.activeFinder = e;
-  }
+
 }
