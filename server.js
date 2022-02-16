ff2d73173dba76511c1840a569b3b13d5b86ba9e
ANGEL IVAN REYES HERNANDEZ <BRM15603@da.banregio.com>
2022-02-16 11:18:41 -0600

    refactorizacion

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static('./dist/nx-getting-started-ui'));
-app.get("/*",function(req, res){
-    res.sendFile("index.html",{root: './dist/nx-getting-started-ui/'});
+const port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.send('Invalid endpoint');
 });
 
-app.listen(process.env.PORT || 8080);
-console.log('running in ${process.env.PORT || 8080}');
+app.listen(port, () => {
+    console.log("Server started on port: " + port)
+});
