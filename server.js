8491328b3c8677927effa82fdf44379d1754f0c6
ANGEL IVAN REYES HERNANDEZ <BRM15603@da.banregio.com>
2022-02-16 11:24:30 -0600

    change dist

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/', (req, res) => {
     res.send('Invalid endpoint');
