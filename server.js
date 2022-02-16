4f7c0f8be4a1f2bfc18842235d93599089b1752c
ANGEL IVAN REYES HERNANDEZ <BRM15603@da.banregio.com>
2022-02-15 21:18:17 -0600

    add dist

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static('./dist/aplicacionEmpresarial'));
+app.use(express.static('./dist/jolly-roger-aplicacation'));
 app.get("/*",function(req, res){
-    res.sendFile("index.html",{root: './dist/aplicacionEmpresarial/'});
+    res.sendFile("index.html",{root: './dist/jolly-roger-aplicacation/'});
 });
 
 app.listen(process.env.PORT || 8080);
