5834ea10d29dbfc1ddb305a629bfc0dffcef9cd5
ANGEL IVAN REYES HERNANDEZ <BRM15603@da.banregio.com>
2022-02-16 11:28:12 -0600

    new route for build

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'nx-getting-started-ui')));
 
 app.get('/', (req, res) => {
     res.send('Invalid endpoint');
